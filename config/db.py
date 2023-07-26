2afb493d243e8c9511e1740d72bba8c49b9cbc5a
Gabriela Calzadilla <gabvero23@gmail.com>
2023-07-26 13:10:59 +0200

    Opcion de descarga info estudiantes en archivo csv

--- config/db.py
+++ config/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root:root@localhost:3306/proyectofenix2")
+engine = create_engine("mysql+pymysql://root:incorrecto@localhost:3306/proyectofenix")
 
-engine.execute(f"CREATE DATABASE IF NOT EXISTS proyectofenix2;")
+engine.execute(f"CREATE DATABASE IF NOT EXISTS proyectofenix;")
 
 meta_data = MetaData()
